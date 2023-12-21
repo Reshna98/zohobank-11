bb27f1eea435cd535ad29f8ab2e6e0165a3ba46e
unknown <reshnasuresh567@gmail.com>
2023-12-21 22:55:02 +0530

    9

--- zohoapp/views.py
+++ zohoapp/views.py
@@ -12743,11 +12743,51 @@ def create_bank(request):
             cp= company_details.objects.get(user = request.user)
             return render(request, 'addbank.html', {'company':cp})
 
+# def edit_bank(request, bank_id):
+#     if request.user.is_authenticated:
+#         bank = Bankcreation.objects.get(id=bank_id, user=request.user)
+#         if request.method == 'POST':
+#             # date = request.POST.get('date')
+#             date_str = request.POST.get('date')
+#             date = datetime.strptime(date_str, '%Y-%m-%d').date() if date_str else None
+#             name = request.POST.get('name')
+#             opn_bal_str = request.POST.get('opn_bal', '')
+#             bal_type = request.POST.get('bal_type')
+#             branch = request.POST.get('branch')
+#             ac_no = request.POST.get('ac_no')
+#             ifsc = request.POST.get('ifsc')
+
+#             opn_bal = float(opn_bal_str) if opn_bal_str.strip() else 0.0
+
+#             if opn_bal == 0.0:
+#                 bal_type = 'Zero Balance'
+
+#             if bal_type == 'Credit':
+#                 opn_bal = -opn_bal
+#             bank.name = name
+#             bank.opn_bal = opn_bal
+#             bank.bal_type = bal_type
+#             bank.branch = branch
+#             bank.ac_no = ac_no
+#             bank.ifsc = ifsc
+#             bank.date = date
+#             bank.balance = opn_bal
+#             bank.save()
+
+#             for transaction in transactions.objects.filter(bank=bank, type="Opening Balance"):
+#                 transaction.amount = opn_bal
+#                 transaction.date = date
+#                 transaction.save()
+
+#             return redirect('bank_listout' ,id= bank_id )
+
+#         else:
+#             cp = company_details.objects.get(user=request.user)
+#             return render(request, 'editbank.html', {'company': cp, 'bank': bank})
 def edit_bank(request, bank_id):
     if request.user.is_authenticated:
         bank = Bankcreation.objects.get(id=bank_id, user=request.user)
         if request.method == 'POST':
-            # date = request.POST.get('date')
             date_str = request.POST.get('date')
             date = datetime.strptime(date_str, '%Y-%m-%d').date() if date_str else None
             name = request.POST.get('name')
@@ -12764,27 +12804,34 @@ def edit_bank(request, bank_id):
 
             if bal_type == 'Credit':
                 opn_bal = -opn_bal
-            bank.name = name
+            
+            balance_difference = opn_bal - bank.opn_bal
+
             bank.opn_bal = opn_bal
+            bank.name = name
             bank.bal_type = bal_type
             bank.branch = branch
             bank.ac_no = ac_no
             bank.ifsc = ifsc
             bank.date = date
-            bank.balance = opn_bal
+            bank.balance += balance_difference
             bank.save()
 
-            for transaction in transactions.objects.filter(bank=bank, type="Opening Balance"):
-                transaction.amount = opn_bal
-                transaction.date = date
+            opening_balance_transaction = transactions.objects.get(bank=bank, type="Opening Balance")
+            opening_balance_transaction.amount = opn_bal
+            opening_balance_transaction.date = date
+            opening_balance_transaction.balance += balance_difference
+            opening_balance_transaction.save()
+
+            all_transactions = transactions.objects.filter(bank=bank).exclude(type="Opening Balance")
+            for transaction in all_transactions:
+                transaction.balance += balance_difference
                 transaction.save()
 
-            return redirect('bank_listout' ,id= bank_id )
-
+            return redirect('bank_listout', id=bank_id)
         else:
             cp = company_details.objects.get(user=request.user)
             return render(request, 'editbank.html', {'company': cp, 'bank': bank})
-
 
 def delete_bank(request, bank_id):
     if request.user.is_authenticated:
@@ -13338,18 +13385,36 @@ def edit_bank_to_bank_transfer(request, id):
         return render(request, 'banklistout.html', {'company': cp, 'bank': b})
 
 
-def delete_transaction(request,id):
+# def delete_transaction(request,id):
+#     cp = company_details.objects.get(user=request.user)
+#     transaction = get_object_or_404(transactions, id=id)
+#     transaction.delete()
+#     bank = transaction.bank
+#     bank.balance -= transaction.amount
+#     bank.save()
+#     bank_id = transaction.bank.id
+#     return redirect('bank_listout', id=bank_id)  
+
+#     return render(request, 'banklistout.html', {'transaction':transaction,'company':cp})
+
+def delete_transaction(request, id):
     cp = company_details.objects.get(user=request.user)
     transaction = get_object_or_404(transactions, id=id)
-    transaction.delete()
+    
     bank = transaction.bank
     bank.balance -= transaction.amount
     bank.save()
+
+    # Deduct the transaction amount from all transactions related to this bank
+    related_transactions = transactions.objects.filter(bank=bank).exclude(type="Opening Balance")
+    for related_transaction in related_transactions:
+        related_transaction.balance -= transaction.amount
+        related_transaction.save()
+
     bank_id = transaction.bank.id
-    return redirect('bank_listout', id=bank_id)  
-
-    return render(request, 'banklistout.html', {'transaction':transaction,'company':cp})
-
+    transaction.delete()
+    
+    return redirect('bank_listout', id=bank_id)
 
 
 def bank_attachfile(request,id):
